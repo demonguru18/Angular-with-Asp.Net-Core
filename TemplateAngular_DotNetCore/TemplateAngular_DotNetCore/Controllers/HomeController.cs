141fac79b1088bdd810a8d208d0dd7bb055897dd
Silvair Leite Soares <silvairsoares@outlook.com>
2018-08-07 12:04:32 -0300

    Update templante for IIS 7.5

--- TemplateAngular_DotNetCore/TemplateAngular_DotNetCore/Controllers/HomeController.cs
+++ TemplateAngular_DotNetCore/TemplateAngular_DotNetCore/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using TemplateAngular_DotNetCore.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace TemplateAngular_DotNetCore.Controllers
 {
@@ -13,31 +7,6 @@ namespace TemplateAngular_DotNetCore.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
